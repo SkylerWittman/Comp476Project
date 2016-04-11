7f48be8130a8930d75058f3953ea91ec585dfe9f
Skyler Wittman <skyler.wittman@gmail.com>
2016-04-11 18:21:54 -0400

    dinos have a bit more health

--- Assets/Scripts/WaveEngine.cs
+++ Assets/Scripts/WaveEngine.cs
@@ -62,7 +62,7 @@ public class WaveEngine : MonoBehaviour {
                     dinoInstance.transform.localScale = new Vector3(randSize, randSize, randSize);
                     dinoInstance.GetComponent<DinoBehavior>().maxSpeed = 11.0f - randSize;
                     dinoInstance.GetComponent<DinoBehavior>().maxRunSpeed = 16.0f - randSize;
-                    dinoInstance.GetComponent<NPCDetail>().health = 20.0f * randSize;
+                    dinoInstance.GetComponent<NPCDetail>().health = 25.0f * randSize;
                     dinoInstance.GetComponent<NPCDetail>().damage = 2.0f * randSize;
                     break;
                 case 2:
